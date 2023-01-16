17a88607f506b602909085d91f2520b924c9f24a
Liam Thorne <lnthorne@shaw.ca>
2023-01-15 20:03:06 -0800

    Clean up InfoCard

--- meal-time/App.tsx
+++ meal-time/App.tsx
@@ -2,9 +2,18 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./src/infrastructure/theme";
 
+import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { ResturantScreen } from "./src/features/screens/resturants.screen";
 
 export default function App() {
+	const [oswaldLoaded] = useOswald({
+		Oswald_400Regular,
+	});
+
+	const [latoLoaded] = useLato({
+		Lato_400Regular,
+	});
 	return (
 		<>
 			<ThemeProvider theme={theme}>
--- meal-time/src/features/components/resturant-info-card.component.tsx
+++ meal-time/src/features/components/resturant-info-card.component.tsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { View } from "react-native";
 import { Text, Card } from "react-native-paper";
-import { Image } from "react-native";
+import { SvgXml } from "react-native-svg";
 import styled from "styled-components/native";
+import open from "../../../assets/open";
+import star from "../../../assets/star";
 
 import { Resturant } from "../../typings/resturant.interface";
 import { GetResturantInfo } from "../hooks/resturant-info.hook";
@@ -10,28 +13,77 @@ const ResturantCard = styled(Card)`
 	background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
-const ResturantImage = styled(Image)`
-	width: 100%;
-	height: 200px;
+const RestaurantCardCover = styled(Card.Cover)`
+	padding: ${(props) => props.theme.space[3]};
+	background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
-const Title = styled(Text)`
-	color: ${(props) => props.theme.colors.ui.primary};
+const Info = styled(View)`
 	padding: ${(props) => props.theme.space[3]};
 `;
 
-export const ResturantInfoCard = () => {
-	const data: Array<Resturant> = GetResturantInfo();
+const Title = styled(Text)`
+	font-family: ${(props) => props.theme.fonts.body};
+	font-size: ${(props) => props.theme.fontSizes.body};
+	color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Rating = styled(View)`
+	flex-direction: row;
+	padding-top: ${(props) => props.theme.space[2]};
+	padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const TempCloseWarning = styled(Text)`
+	font-family: ${(props) => props.theme.fonts.heading};
+	font-size: ${(props) => props.theme.fontSizes.body};
+	font-weight: bold;
+	color: red;
+
+	margin-left: auto;
+`;
+
+const OpenIcon = styled(View)`
+	margin-left: auto;
+`;
+
+const Address = styled(Text)`
+	font-family: ${(props) => props.theme.fonts.body};
+	font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+export const ResturantInfoCard = (restaurant: any = {}) => {
+	// const data: Array<Resturant> = GetResturantInfo();
+	const {
+		name = "Some Restaurant",
+		icon,
+		photos = [
+			"https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+		],
+		address = "100 some random street",
+		isOpenNow = true,
+		rating = 4,
+		isClosedTemporarily = false,
+	} = restaurant;
+
+	const ratingArray = Array.from(new Array(Math.floor(rating)));
+
 	return (
 		<ResturantCard elevation={5}>
-			<ResturantImage
-				source={{
-					uri: "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
-				}}
-			/>
-			<Title>{data[0].name}</Title>
-			<Title>{data[0].address}</Title>
-			<Title>{data[0].icon}</Title>
+			<RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+			<Info>
+				<Title>{name}</Title>
+				<Rating>
+					{ratingArray.map(() => (
+						<SvgXml xml={star} width={20} height={20} />
+					))}
+					{isClosedTemporarily && <TempCloseWarning>Temporarily Closed</TempCloseWarning>}
+					<OpenIcon>
+						{isOpenNow && !isClosedTemporarily && <SvgXml xml={open} width={20} height={20} />}
+					</OpenIcon>
+				</Rating>
+				<Address>{address}</Address>
+			</Info>
 		</ResturantCard>
 	);
 };
